c504ebd2ddb6d90e7f08d45fae3cce1f54fe3a79
ShadelessFox <yanuari.channel@gmail.com>
2026-09-15 03:09:40 +0200

    Add debug renderers to improve debug experience

--- src/main/java/com/shade/decima/rpcs3/memory/Ref.java
+++ src/main/java/com/shade/decima/rpcs3/memory/Ref.java
@@ -1,5 +1,8 @@
 package com.shade.decima.rpcs3.memory;
 
+import org.jetbrains.annotations.Debug;
+
+@Debug.Renderer(childrenArray = "this.read()", hasChildren = "this.pointer().address() != 0L")
 public record Ref<T>(Type<T> type, Pointer pointer) {
     public T read() {
         return type.read(pointer);
--- src/main/java/com/shade/decima/rpcs3/memory/Slice.java
+++ src/main/java/com/shade/decima/rpcs3/memory/Slice.java
@@ -1,9 +1,12 @@
 package com.shade.decima.rpcs3.memory;
 
+import org.jetbrains.annotations.Debug;
+
 import java.util.Iterator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+@Debug.Renderer(childrenArray = "this.stream().toArray()", hasChildren = "this.length > 0")
 public record Slice<T>(Type.Sized<T> type, Pointer pointer, int length) implements Iterable<T> {
     public T read(int index) {
         if (index < 0 || index >= length) {
